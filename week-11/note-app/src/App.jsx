5a46c00abe0fe79ca2daa2af59aaf3631306784f
Gökhan Cihan <83141061+GokhanCihan@users.noreply.github.com>
2024-08-27 19:32:58 +0300

    Palette comp

--- week-11/note-app/src/App.jsx
+++ week-11/note-app/src/App.jsx
@@ -1,40 +1,15 @@
+import { useState } from 'react';
 import './App.css'
 import NewNote from './Components/NewNote/NewNote';
 import Notes from './Components/Notes/Notes'
-
+import DATA from './assets/DATA';
 function App() {
-  const DATA = [
-    {
-      id: 1,
-      note: "Hello World! asda sadsadf otuz kırk sekizyüz elli kırkbeş 21 652 ssda zxc 6qqe zz qwe xxxx vxa x",
-      color: "red",
-    },
-    {
-      id: 2,
-      note: "Note 2 asdacczasdasdas asd ",
-      color: "yellow",
-    },
-    {
-      id: 3,
-      note: "Note 3 asdasd sss xczx aw2 vs",
-      color: "blue",
-    },
-    {
-      id: 4,
-      note: "Note 499799797979797997996666666666",
-      color: "green",
-    },
-    {
-      id: 5,
-      note: "Note 5",
-      color: "blue",
-    }
-  ];
+  const [notes, setNotes] = useState([...DATA])
   return (
     <>
      <h1>NOTES APP</h1>
-     <NewNote />
-     <Notes data={DATA} />
+     <NewNote  data={notes} setData={setNotes}/>
+     <Notes data={notes} />
     </>
   )
 }
--- week-11/note-app/src/Components/NewNote/NewNote.jsx
+++ week-11/note-app/src/Components/NewNote/NewNote.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState, uuid } from 'react'
 import './NewNote.css'
+import Palette from './Palette/Palette'
 
-function NewNote(props) {
+function NewNote({ data, setData }) {
+  const [color, setColor] = useState(null);
+  const [note, setNote] = useState("");
+  const [uid, setUid] = useState(6);
+
+  const handleChange = (e) => {
+    setNote(e.target.value)
+  }
+
+  const addNewNote = () => {
+    setUid(prev => prev + 1);
+    setData(prev => [...prev, {id: uid + 1, note: note, color: color}])
+  }
+
+  useEffect(() => {
+    return () => {
+      console.log(data, color, note, uid);
+    }
+  },)
+
   return (
     <div className='new-note'>
-      <textarea id="note-area" placeholder="Enter your note here..."></textarea>
+      <textarea id="note-area" placeholder="Enter your note here..." value={note} onChange={handleChange}></textarea>
       <div className='controls'>
-        <div className='palette'>Color Palette</div>
-        <button className='add-btn'>ADD</button>
+        <Palette setColor={setColor}/>
+        <button className='add-btn' onClick={addNewNote}>ADD</button>
       </div>
     </div>
   )
