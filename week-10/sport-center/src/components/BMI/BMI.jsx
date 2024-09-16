4695170ed19f3369aa93b7e0e4cf964c6efafe33
Gökhan Cihan <83141061+GokhanCihan@users.noreply.github.com>
2024-09-16 13:32:40 +0300

    -add bmi logic

--- week-10/sport-center/src/components/BMI/BMI.jsx
+++ week-10/sport-center/src/components/BMI/BMI.jsx
@@ -1,8 +1,40 @@
 import './BMI.style.css'
 import bmi from '../../assets/images/bmi-index.jpg'
+import { useEffect, useRef, useState } from 'react';
 
 function BMI() {
+  const [values, setValues] = useState({"height": null, "weight": null});
+  const indexRef = useRef(null);
+
+  const handleHeightChange = (e) => {
+    setValues({...values,"height": e.target.value});
+  }
   
+  const handleWeightChange = (e) => {
+    setValues({...values, "weight": e.target.value});
+  }
+
+  useEffect(() => {
+    if(values.height>0 && values.weight>0) {
+      const result = 10_000 * values.weight / (values.height ** 2);
+      if(result<=18.5) {
+        indexRef.current.style.left = "13%";
+      }
+      if (result > 18.5 && result <= 24.9) {
+        indexRef.current.style.left = "29%";
+      }
+      if (result > 24.9 && result <= 29.9) {
+        indexRef.current.style.left = "45%";
+      }
+      if (result > 29.9 && result <= 34.9) {
+        indexRef.current.style.left = "61%";
+      }
+      if (result > 34.9) {
+        indexRef.current.style.left = "77%";
+      }
+    }
+  }, [values]);
+
   return (
     <section id="bmi">
       <div className="content-area">
@@ -14,11 +46,11 @@ function BMI() {
             classical latin literature from 45 BC, making it over 2000 years old.</p>
           <div id="inputs">
             <div>
-              <input id="height" type="text" placeholder="Your Height" />
+              <input onChange={handleHeightChange} id="height" type="text" placeholder="Your Height" />
               <span>cm</span>
             </div>
             <div>
-              <input id="weight" type="text" placeholder="Your Weight" />
+              <input onChange={handleWeightChange} id="weight" type="text" placeholder="Your Weight" />
               <span>kg</span>
             </div>
           </div>
@@ -26,7 +58,7 @@ function BMI() {
         <div id="bmi-index">
           <h3>Your BMI</h3>
           <img src={bmi} alt="bmi index" />
-          <div id="bmi-indicator" className="triangle"></div>
+          <div ref={indexRef} id="bmi-indicator" className="triangle"></div>
         </div>
       </div>
     </section>
